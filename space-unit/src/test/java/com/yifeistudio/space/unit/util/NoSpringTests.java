ef7c9cd1b1f0a2fc127b462f0671157bb356d5d7
弘毅 <develop@yifeistudio.com>
2022-05-09 23:25:35 +0800

    unit 编写

--- space-unit/src/test/java/com/yifeistudio/space/unit/util/NoSpringTests.java
+++ space-unit/src/test/java/com/yifeistudio/space/unit/util/NoSpringTests.java
@@ -2,6 +2,11 @@ package com.yifeistudio.space.unit.util;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 /**
  * 无Spring 环境测试
@@ -14,9 +19,19 @@ class NoSpringTests {
     @Test
     void jvmTest() {
         int i = Runtime.getRuntime().availableProcessors();
-
         System.out.println(i);
+    }
 
+    @Test
+    void splitTest() {
+        int size = 100000000;
+        List<Integer> x = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            x.add(i);
+        }
+        long l = System.currentTimeMillis();
+        List<List<Integer>> split = Spliter.split(x, 100);
+        System.out.println(System.currentTimeMillis() - l);
     }
 
 }
