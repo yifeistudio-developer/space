02718f80c358d1d1844168223dfd6dc244b8bca2
弘毅 <develop@yifeistudio.com>
2022-08-10 19:52:35 +0800

    update version.

--- space-unit/build.gradle.kts
+++ space-unit/build.gradle.kts
@@ -18,13 +18,12 @@ repositories {
 
 dependencies {
 
-    compileOnly("com.fasterxml.jackson.core:jackson-core:2.13.2")
-    compileOnly("com.fasterxml.jackson.core:jackson-databind:2.13.2.2")
-
-    testRuntimeOnly("com.fasterxml.jackson.core:jackson-core:2.13.2")
-    testRuntimeOnly("com.fasterxml.jackson.core:jackson-databind:2.13.2.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    compileOnly("com.fasterxml.jackson.core:jackson-core:2.13.3")
+    compileOnly("com.fasterxml.jackson.core:jackson-databind:2.13.3")
+    testRuntimeOnly("com.fasterxml.jackson.core:jackson-core:2.13.3")
+    testRuntimeOnly("com.fasterxml.jackson.core:jackson-databind:2.13.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
 
 }
 
