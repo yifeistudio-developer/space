41ad776262b6780c13826b66fbeb64575f03ed0d
弘毅 <develop@yifeistudio.com>
2022-09-13 16:46:59 +0800

    代码优化

---
+++ space-starter/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+/**
+ * Starter 模块
+ *
+ * @author : hongyi
+ * created at 2022/9/13 - 16:45
+ **/
+module space.space.starter {
+    requires lombok;
+    requires spring.boot;
+    requires spring.context;
+    requires spring.core;
+    requires spring.beans;
+
+
+}
