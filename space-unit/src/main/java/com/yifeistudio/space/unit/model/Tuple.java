25987fc5a51859e27818e8af0b0d85511192d44d
弘毅 <develop@yifeistudio.com>
2022-09-14 15:34:07 +0800

    模块化

--- space-unit/src/main/java/com/yifeistudio/space/unit/model/Tuple.java
+++ space-unit/src/main/java/com/yifeistudio/space/unit/model/Tuple.java
@@ -8,37 +8,10 @@ import java.io.Serializable;
  * @author : hongyi
  * created at 2022/4/22 - 10:52
  **/
-public class Tuple<L, R> implements Serializable {
-
-    private L left;
-    private R right;
-
-    public Tuple(L left, R right) {
-        this.left = left;
-        this.right = right;
-    }
+public record Tuple<L, R>(L left, R right) implements Serializable {
 
     public static <T, R> Tuple<T, R> of(T left, R right) {
         return new Tuple<>(left, right);
     }
 
-
-    //---------- getter setter ----------
-
-
-    public L getLeft() {
-        return left;
-    }
-
-    public void setLeft(L left) {
-        this.left = left;
-    }
-
-    public R getRight() {
-        return right;
-    }
-
-    public void setRight(R right) {
-        this.right = right;
-    }
 }
