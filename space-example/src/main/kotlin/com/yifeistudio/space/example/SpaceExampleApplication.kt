039046b923e86ac2c242d50a0ea0c1e53fae7bd6
弘毅 <develop@yifeistudio.com>
2022-10-20 20:43:04 +0800

    result config.

--- space-example/src/main/kotlin/com/yifeistudio/space/example/SpaceExampleApplication.kt
+++ space-example/src/main/kotlin/com/yifeistudio/space/example/SpaceExampleApplication.kt
@@ -1,10 +1,8 @@
 package com.yifeistudio.space.example
 
-import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@NacosPropertySource(autoRefreshed = true, dataId = "space-example-config")
 @SpringBootApplication
 class SpaceExampleApplication
 
