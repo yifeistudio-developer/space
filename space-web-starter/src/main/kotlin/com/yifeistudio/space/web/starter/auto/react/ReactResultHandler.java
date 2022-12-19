305afad31800da564a471d31b9f0579842902561
弘毅 <develop@yifeistudio.com>
2022-12-19 15:10:01 +0800

    项目结构调整

---
+++ space-web-starter/src/main/kotlin/com/yifeistudio/space/web/starter/auto/react/ReactResultHandler.java
@@ -0,0 +1,10 @@
+package com.yifeistudio.space.web.starter.auto.react;
+
+/**
+ * File Description
+ *
+ * @author : hongyi
+ * created at 2022/12/19 - 15:07
+ **/
+public class ReactResultHandler {
+}
---
+++ space-web-starter/src/main/kotlin/com/yifeistudio/space/web/starter/auto/servlet/ServletResultHandler.java
@@ -0,0 +1,11 @@
+package com.yifeistudio.space.web.starter.auto.servlet;
+
+/**
+ * File Description
+ *
+ * @author : hongyi
+ * created at 2022/12/19 - 15:07
+ **/
+public class ServletResultHandler {
+
+}
