7de592ab7275ff0f18dcc5bd448e034aea5711c0
弘毅 <develop@yifeistudio.com>
2022-12-29 14:21:14 +0800

    项目结构调整

--- space-starter/src/test/kotlin/com/yifeistudio/space/starter/SpaceStarterApplicationTests.kt
+++ space-starter/src/test/kotlin/com/yifeistudio/space/starter/SpaceStarterApplicationTests.kt
@@ -4,7 +4,7 @@ import com.yifeistudio.space.starter.auto.SpringContextHelper
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+@SpringBootTest(classes = [ApplicationTests::class])
 class SpaceStarterApplicationTests {
 
     @Test
