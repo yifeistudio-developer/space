259019fa25be3dfe8ae32fda9f5cb01d4bd2c9f1
弘毅 <develop@yifeistudio.com>
2023-01-03 09:26:38 +0800

    add tests.

---
+++ space-web-starter/src/test/java/com/yifeistudio/web/starter/SpaceWebStarterApplicationTests.java
@@ -0,0 +1,17 @@
+package com.yifeistudio.web.starter;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * File Description
+ *
+ * @author : hongyi
+ * created at 2023/1/2 - 22:06
+ **/
+@SpringBootTest
+class SpaceWebStarterApplicationTests {
+
+
+
+
+}
