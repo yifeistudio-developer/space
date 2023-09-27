267dbe6cf5fe761d1008b053a1c730c384b1573b
hongyi <develop@yifeistudio.com>
2023-09-27 10:54:29 +0800

    模块打包插件调整

--- space-unit/build.gradle.kts
+++ space-unit/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 val ossrhUsername: String by project
 val ossrhPassword: String by project
 
-version = "2.0.2-SNAPSHOT"
+version = "2.0.3-SNAPSHOT"
 
 val jacksonVersion = "2.15.2"
 val junitVersion = "5.10.0"
